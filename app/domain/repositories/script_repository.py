0f2d24dd8cbdfb2c0041587e971a2452f99ace54
Amalnath <amalnaths640@gmail.com>
2026-01-07 14:11:48 +0530

    Added Script Repository

---
+++ app/domain/repositories/script_repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from app.domain.entities.script import Script
+
+
+
+class ScriptRepository(ABC):
+    
+    @abstractmethod
+    async def save(self, script: "Script") -> None:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self, script_id: str) -> Optional["Script"]:
+        pass
+    
+    @abstractmethod
+    async def update_status(self, script_id: str, new_status: str) -> None:
+        pass
+
+    @abstractmethod
+    async def delete(self, script_id: str) -> None:
+        pass
+
+    @abstractmethod
+    async def list_all(self) -> List["Script"]:
+        pass
